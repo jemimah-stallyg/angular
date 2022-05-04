8a256c6aa0befe0bf792a943bba55aadcf1c7ff6
jemimah-stallyg <jemimah.s@iinterchange.com>
2022-05-04 11:20:04 +0530

    modified file

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { features } from 'process';
 // import { EventEmitter } from 'stream';
 
